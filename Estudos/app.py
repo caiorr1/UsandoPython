071f4e532637e27f9210fa87b94c73e33f19b276
Caio Ribeiro Rodrigues <caiorrodrigues2004@gmail.com>
2023-05-16 17:47:12 -0300

    uPTADE

--- Estudos/app.py
+++ Estudos/app.py
@@ -1,34 +1,50 @@
 # Mode
 # r - Leitura
-# a - Append  / Incrementar
-#  w - Escrita
+# a - Append  / Incrementar -> Continua colocando no arquivo que ja existe
+#  w - Escrita -> Limpa o arquivo ou até criar um arquivo novo
 # x - Criar Arquivo
 # r+ - Leitura + Escrita
 
 # Abre o arquivo, e escolhe o modo
-arquivo = open("Estudos/teste.txt", "a")
+arquivo = open("Estudos/teste3.txt", "x")
 
 # Verifica se o arquivo é legível
-#print(arquivo.readable())
+print(arquivo.readable())
 
 # Lê o arquivo
-#print(arquivo.read())
+print(arquivo.read())
 
 # Lê linha por linha do arquivo
-#print(arquivo.readline())
-#print(arquivo.readline())
-#print(arquivo.readline())
+print(arquivo.readline())
+print(arquivo.readline())
+print(arquivo.readline())
 
 # Pega todas as linhas do arquivo e transforma em uma lista
-# lista = arquivo.readlines()
-# print(lista)
+lista = arquivo.readlines()
+print(lista)
 # Lê apenas um resultado
-# print(lista[3])
+print(lista[3])
 
 
 # Incrementar coisas no Arquivo
-arquivo.write("\nSQL")
+arquivo.write("Python\n")
+arquivo.write("C++\n")
+arquivo.write("Terraform\n")
+
 
 
 # Fecha o Arquivo
 arquivo.close()
+
+# Exclui arquivo
+import os 
+os.remove("Estudos/teste2.txt")
+
+# Verificar se o arquivo existe
+if os.path.exists("Estudos/teste3.txt"):
+    os.remove("Estudos/teste3.txt")
+else:
+    print ("Arquivo não existe")
+
+# Exclui pasta
+os.rmdir("Estudos/nova pasta")
