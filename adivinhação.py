d91ba81df2ca5a98501b35aabd7b6af09c76ba55
Caio Ribeiro Rodrigues <caiorrodrigues2004@gmail.com>
2023-05-16 00:39:20 -0300

    uptade

--- adivinhação.py
+++ adivinhação.py
@@ -1,26 +1,33 @@
+import random
+
 print('*********************************')
 print('Bem vindo ao Jogo de Adivinhação!')
 print('*********************************')
 
-num_secreto = '13'
-num_user = input('Digite o seu número: ')
+tentativas = 3
+num_secreto = random.randrange(1,101) 
 
-print(f'Você digitou {num_user}')
 
-if num_user == num_secreto:
-    print('-----------------------')
-    print('PARABENSSSSSSSSSSSSSSSS')
-    print('-----------------------')
-    print('Você acertou o número!')
-else:
-    while num_user != num_secreto:
-        print('Você errou, tente novamente')
-        num_user = input('Digite o seu número: ')
-        if num_user == num_secreto:
-            print('-----------------------')
-            print('PARABENSSSSSSSSSSSSSSSS')
-            print('-----------------------')
-            print('Você acertou o número!')
-        else:
-            ('Errou')
-        
+for rodada in range (1, tentativas + 1):  
+    print(f'Tentativa {rodada} de {tentativas}')
+    num_user = input('Digite um número entre 1 e 100: ')
+    chute = int(num_user)
+    if chute < 0 or chute > 100:
+        print('Você deve digitar um número de 1 a 100')
+        continue
+    
+    print(f'Você digitou {num_user}')
+    acertou = chute == num_secreto
+    maior = chute > num_secreto
+    menor = chute < num_secreto
+
+    if(acertou):
+        print("Parabéns! Você acertou!")
+        break
+    else:
+        if(maior):
+            print("O seu chute foi maior do que o número secreto!")
+        elif(menor):
+            print("O seu chute foi menor do que o número secreto!")
+
+print("Fim do jogo")
