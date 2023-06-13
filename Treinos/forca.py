801f19e72b8207599f593864abe621e6b993a81e
Caio Ribeiro Rodrigues <caiorrodrigues2004@gmail.com>
2023-06-13 19:47:07 -0300

    training

--- Treinos/forca.py
+++ Treinos/forca.py
@@ -3,6 +3,17 @@ def jogo_forca():
     print("Bem vindo ao Jogo de Forca!")
     print("*********************************")
 
+    arquivo = open('palavras.txt', 'r')
+    palavras = []
+    
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+        
+    arquivo.close()
+    
+    print(palavras)
+
     palavra_secreta = "banana".upper()
     letras_acertadas = ['_'for letra in palavra_secreta]
     
---
+++ Treinos/tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+arquivo = open('palavras.txt', 'r')
+    palavras = []
+    
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+        
+    arquivo.close()
+    
+    print(palavras)
