07cad27f7fbd70fb663f4157ce6b75e390b2931f
caiorr1 <caiorrodrigues2004@gmail.com>
2023-07-04 13:36:06 -0300

    uptade

---
+++ Estudos/Alura-PythonOO/começo.py
@@ -0,0 +1,18 @@
+
+def cria_conta (numero,titular,saldo,limite):
+    conta = {
+    "numero": numero, 
+    "titular": titular,
+    "saldo" : saldo,
+    "limite" : limite
+    }
+    return conta
+
+def depositar(conta,valor):
+    conta["saldo"] += valor
+
+def saque(conta,valor):
+    conta['saldo'] -= valor
+    
+def extrato(conta):
+    print(f"Seu saldo é:{conta['saldo']}")
--- Treinos/treinoautomacao/main.py
+++ Treinos/treinoautomacao/main.py
@@ -6,15 +6,15 @@ import time
 from anticaptchaofficial.recaptchav2proxyless import *
 
 navegador = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-link = "https://google.com/recaptcha/api2/demo"
+site = "https://google.com/recaptcha/api2/demo"
 
-navegador.get(link)
+navegador.get(site)
 chave_captcha = navegador.find_element(By.ID, 'recaptcha-demo').get_attribute('data-sitekey')
 
 solver = recaptchaV2Proxyless()
 solver.set_verbose(1)
 solver.set_key() # -> chave api da sua conta
-solver.set_website_url(link) # -> link do site
+solver.set_website_url(site) # -> link do site
 solver.set_website_key() # -> pegar data-sitekey
 
 resposta = solver.solve_and_return_solution()
